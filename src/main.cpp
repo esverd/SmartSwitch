4d49304453800eef7f849f42f6ea1a774c3c5cd2
esverd <eirik.sverd@gmail.com>
2021-04-11 14:57:33 +0200

    before switching to esp8266 uc

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int val;    // variable to read the value from the analog pin
 
 void setup() 
 {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(6);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() 
@@ -18,3 +18,50 @@ void loop()
   myservo.write(val);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
 }
+
+
+/* Example sketch to control a 28BYJ-48 stepper motor with ULN2003 driver board and Arduino UNO. More info: https://www.makerguides.com */
+// Include the Arduino Stepper.h library:
+// #include <Arduino.h>
+// #include <Stepper.h>
+// // Define number of steps per rotation:
+// const int stepsPerRevolution = 2048;
+// // Wiring:
+// // Pin 8 to IN1 on the ULN2003 driver
+// // Pin 9 to IN2 on the ULN2003 driver
+// // Pin 10 to IN3 on the ULN2003 driver
+// // Pin 11 to IN4 on the ULN2003 driver
+// // Create stepper object called 'myStepper', note the pin order:
+// Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+// int potpin = 0;  // analog pin used to connect the potentiometer
+// int val;    // variable to read the value from the analog pin
+
+// void setup() {
+//   // Set the speed to 5 rpm:
+//   myStepper.setSpeed(5);
+//   // Begin Serial communication at a baud rate of 9600:
+//   Serial.begin(9600);
+
+//   pinMode(potpin, INPUT);
+// }
+// void loop() {
+
+//   val = analogRead(potpin);
+//   if(val < 300)
+//     myStepper.step(1);
+//   else if(val > 700)
+//     myStepper.step(-1);
+  
+//   delay(5);
+
+//   // Step one revolution in one direction:
+//   // Serial.println("clockwise");
+//   // myStepper.step(stepsPerRevolution);
+//   // delay(500);
+  
+//   // // Step one revolution in the other direction:
+//   // Serial.println("counterclockwise");
+//   // myStepper.step(-stepsPerRevolution);
+//   // delay(500);
+// }
